1cd11b948487aebbd0e7eab6f81e0b64a69695a2
Mayank Diwakar <78422111+flutterbuddy1@users.noreply.github.com>
2026-05-18 14:13:31 +0530

    Update service.jsx

--- frontend/src/api/service.jsx
+++ frontend/src/api/service.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://team-management-project-production.up.railway.app/api";
 
 const API = axios.create({
   baseURL: BASE_URL
